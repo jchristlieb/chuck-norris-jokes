7025a9eb2d450fde8808a659f271f86cede7350e
Jan Christlieb <mail@janchristlieb.de>
2019-05-07 09:56:45 +0000

    Apply fixes from StyleCI

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -3,10 +3,10 @@
 namespace Jchristlieb\ChuckNorrisJokes\Tests;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Handler\MockHandler;
 use Jchristlieb\ChuckNorrisJokes\JokeFactory;
 
 class JokeFactoryTest extends TestCase
@@ -16,13 +16,12 @@ class JokeFactoryTest extends TestCase
     {
         // mock the expected request
         $mock = new MockHandler([
-            new Response(200, [],'{ "type": "success", "value": { "id": 154, "joke": "Movie trivia: The movie &quot;Invasion U.S.A.&quot; is, in fact, a documentary.", "categories": [] } }'),
+            new Response(200, [], '{ "type": "success", "value": { "id": 154, "joke": "Movie trivia: The movie &quot;Invasion U.S.A.&quot; is, in fact, a documentary.", "categories": [] } }'),
         ]);
 
         // handle the mock request through guzzle client
         $handler = HandlerStack::create($mock);
         $client = new Client(['handler' => $handler]);
-
 
         $jokes = new JokeFactory($client);
 
@@ -30,5 +29,4 @@ class JokeFactoryTest extends TestCase
 
         $this->assertSame('Movie trivia: The movie &quot;Invasion U.S.A.&quot; is, in fact, a documentary.', $joke);
     }
-
 }
