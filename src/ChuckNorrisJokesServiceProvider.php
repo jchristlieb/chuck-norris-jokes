977612de639482be9ce49c63f9c9a332a1cade5e
Jan Christlieb <mail@janchristlieb.de>
2019-05-08 12:33:54 +0000

    Apply fixes from StyleCI

--- src/ChuckNorrisJokesServiceProvider.php
+++ src/ChuckNorrisJokesServiceProvider.php
@@ -2,10 +2,8 @@
 
 namespace Jchristlieb\ChuckNorrisJokes;
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Jchristlieb\ChuckNorrisJokes\Console\ChuckNorrisJoke;
-use Jchristlieb\ChuckNorrisJokes\Http\Controllers\ChuckNorrisController;
 
 class ChuckNorrisJokesServiceProvider extends ServiceProvider
 {
@@ -18,17 +16,16 @@ class ChuckNorrisJokesServiceProvider extends ServiceProvider
         }
 
         // load web routes
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 
         // load views
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'chuck-norris');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'chuck-norris');
 
         // publish files to vendor project
         // use php artisan vendor:publish to get those published
         $this->publishes([
-            __DIR__ . '/../resources/views' => resource_path('views/vendor/chuck-norris'),
+            __DIR__.'/../resources/views' => resource_path('views/vendor/chuck-norris'),
         ]);
-
     }
 
     public function register()
